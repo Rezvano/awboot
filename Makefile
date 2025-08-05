9cb91502c40a87c5966dc6af07658d81fd6520aa
Rezvano <ivano08.01.1998@gmail.com>
2025-08-05 21:38:25 +0300

    change optimization

--- Makefile
+++ Makefile
@@ -15,7 +15,7 @@ include	arch/arm32/arm32.mk
 include	lib/lib.mk
 
 CFLAGS += -mcpu=cortex-a7 -mthumb-interwork -mthumb -mno-unaligned-access -mfpu=neon-vfpv4 -mfloat-abi=hard
-CFLAGS += -ffast-math -ffunction-sections -fdata-sections -Os -std=gnu99 -Wall -Wno-unused-function -g -MMD $(INCLUDES) $(DEFINES)
+CFLAGS += -ffast-math -ffunction-sections -fdata-sections -O0 -std=gnu99 -Wall -Wno-unused-function -g -MMD $(INCLUDES) $(DEFINES)
 #-Werror
 
 ASFLAGS += $(CFLAGS)
--- boards/board-mangopi-dual-spinand.h
+++ boards/board-mangopi-dual-spinand.h
@@ -34,16 +34,16 @@
 #define CONFIG_DTB_FILENAME	   "sun8i-t113-mangopi-dual.dtb"
 #endif
 
-// #define CONFIG_CMD_LINE_ARGS                                                                                     \
-// 	"mem=64M cma=8M root=/dev/mmcblk0p2 init=/sbin/init console=ttyS5,115200 earlyprintk=sunxi-uart,0x02501400 " \
-// 	"rootwait"
+#define CONFIG_CMD_LINE_ARGS                                                                                     \
+	"mem=128M cma=8M root=/dev/mmcblk0p2 init=/sbin/init console=ttyS3,115200 earlyprintk=sunxi-uart,0x02500c00 " \
+	"rootwait"
 
 #define CONFIG_KERNEL_LOAD_ADDR (SDRAM_BASE + (72 * 1024 * 1024))
 #define CONFIG_DTB_LOAD_ADDR	(SDRAM_BASE + (64 * 1024 * 1024))
 
 // 128KB erase sectors, so place them starting from 2nd sector
-#define CONFIG_SPINAND_DTB_ADDR	   (128 * 2048)
-#define CONFIG_SPINAND_KERNEL_ADDR (256 * 2048)
+#define CONFIG_SPINAND_DTB_ADDR	   (128 * 2048) //262144
+#define CONFIG_SPINAND_KERNEL_ADDR (256 * 2048) //524288
 
 extern sunxi_usart_t USART_DBG;
 extern sunxi_spi_t	 sunxi_spi0;
